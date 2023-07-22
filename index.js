f288a2f41cef4c1a691f159ac5dad198e192efd9
Xavier Minnamon <xaffekt.dev@gmail.com>
2023-07-22 13:37:04 -0700

    Update 1.0
    TODO: improve mobile layout

--- index.js
+++ index.js
@@ -9,37 +9,42 @@ const schemeContainer = document.getElementById('scheme-container-el')
 const colorHexArray = []
 
 
-generateScheme.addEventListener("click", () => {
-    //clears array on button press
-    colorHexArray.splice(0, colorHexArray.length)
-
-    //formats color picker value to work with api query
-    const colorPicker = document.getElementById('color-picker-el').value
-    const colorPickerHex = colorPicker.slice(1,colorPicker.length)
-    console.log(colorPickerHex)
-
-    //Scheme value from select menu
-    const schemeSelect = document.getElementById('scheme-select-el').value
-
-
-    fetch(`https://www.thecolorapi.com/scheme?count=5&hex=${colorPickerHex}&mode=${schemeSelect}`)
-    .then(res => res.json())
-    .then(data => {      
-        data.colors.forEach(element => {
-            colorHexArray.push(element.hex.value)
-        })
-        renderAllColors()
-    })
-    
+document.addEventListener("click", e => {
+    if(e.target === generateScheme) {
+        callColorApi()
+    }
+    else if(e.target.dataset.hexValue) {
+        console.log(e.target.dataset.hexValue)
+        copyHex(e.target.dataset.hexValue)
+    }
 })
 
-function callApi() {
+function callColorApi() {
+        //clears array on button press
+        colorHexArray.splice(0, colorHexArray.length)
+
+        //formats color picker value to work with api query
+        const colorPicker = document.getElementById('color-picker-el').value
+        const colorPickerHex = colorPicker.slice(1,colorPicker.length)
+        console.log(colorPickerHex)
     
+        //Scheme value from select menu
+        const schemeSelect = document.getElementById('scheme-select-el').value
+    
+    
+        fetch(`https://www.thecolorapi.com/scheme?count=5&hex=${colorPickerHex}&mode=${schemeSelect}`)
+        .then(res => res.json())
+        .then(data => {      
+            data.colors.forEach(element => {
+                colorHexArray.push(element.hex.value)
+            })
+            renderAllColors()
+        })
 }
 
 function colorStripeHtml(hex) {
     return `
-    <div class="color-container">
+    <div class="color-container" id=${hex} data-hex-value="${hex}">
         <!-- Background: color -->
         <div class="color-stripe" style="background-color:${hex}"></div>
         <p class="color-code">${hex}</p>
@@ -55,12 +60,9 @@ function renderAllColors() {
     schemeContainer.innerHTML = html
 }
 
+function copyHex(hex) {
+    navigator.clipboard.writeText(hex);
+    alert(`${hex} copied`);
+}
 
-fetch("https://www.thecolorapi.com/scheme?count=5&hex=838383&mode=monochrome")
-    .then(res => res.json())
-    .then(data => {      
-        data.colors.forEach(element => {
-            colorHexArray.push(element.hex.value)
-        })
-        renderAllColors()
-    })
+callColorApi()
